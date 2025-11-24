89f820f77618e9d7fa0ed733c0e443aa1429ace9
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2025-11-24 13:18:21 +0500

    foldering-structure

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,14 +5,12 @@ import { HeaderShownContext } from '@react-navigation/elements';
 
 export default function RootLayout() {
   return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Stack
-          screenOptions={{ headerShown: false }}
-          initialRouteName="index"
-        >
-          <Stack.Screen name='Signup' options={{ headerShown: false }} />
-        </Stack>
-        <Toast />
-      </GestureHandlerRootView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
+        <Stack.Screen name="auth/Signup" options={{ headerShown: false }} />
+        <Stack.Screen name="auth/Login" options={{ headerShown: false }} />
+      </Stack>
+      <Toast />
+    </GestureHandlerRootView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
-
+import { Image, Text,  TouchableOpacity, View } from "react-native"; 
+import { useRouter } from "expo-router";
 export default function Index() {
+  const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Image source={require("../assets/images/saylanilogo.png")} />
+
+      <Text>WELCOME TO</Text>
+      <Text>SAYLANI WELFARE APP</Text>
+
+
+      <TouchableOpacity onPress={() => router.push("/login")}>
+        <Text>Get Started</Text>
+      </TouchableOpacity>
+
+
+      <Text>this app is to easy your application process</Text>
     </View>
   );
 }
